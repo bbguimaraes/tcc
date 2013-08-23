d955022a6e31ca1a5bc92aef18410c7fe812a53c
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2013-08-22 21:36:42 -0300

    Small improvement on auto_make.sh.

--- bin/auto_make.sh
+++ bin/auto_make.sh
@@ -3,9 +3,9 @@
 # Run make every second, but only if necessary. Arguments are forwarded to make
 # -q and make.
 
-make=make\ "$@"
+make=make
 make_test=$make\ -q
 
 while true; do
-    $make_test -q "$@" || $make && sleep 1s
+    $make_test "$@" || $make "$@" && sleep 1s
 done
