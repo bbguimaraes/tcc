895a22dcac3f5b0335a2949f9227275f2a7c99d1
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2013-09-26 21:42:50 -0300

    Added notifications to auto_make.sh.

--- bin/auto_make.sh
+++ bin/auto_make.sh
@@ -7,5 +7,11 @@ make=make
 make_test=$make\ -q
 
 while true; do
-    $make_test "$@" || $make "$@" && sleep 1s
+    if ! $make_test "$@"; then
+        echo making...
+        $make "$@"
+        echo made
+        notify-send -t 1000 make made
+        sleep 1s
+    fi
 done
